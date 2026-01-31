50615fabc86670200d9cadda58148fa3acbf7af3
Hafiz Waseem Ahmed <hafizwaseemahmed2002@gmail.com>
2026-01-31 11:25:14 +0500

    Getting Started with Controllers

--- Express.js/Mega_Project_Youtube_Backend/src/middlewares/multer.middleware.js
+++ Express.js/Mega_Project_Youtube_Backend/src/middlewares/multer.middleware.js
@@ -6,10 +6,10 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.originalname);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
   },
 });
 
-export const upload = multer({ 
-    storage,
- });
+export const upload = multer({
+  storage,
+});
